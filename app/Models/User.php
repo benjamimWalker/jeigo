d5be275ed04a077ede6f24c6da2074617c3b934f
benjamim <bhaskdelta2@gmail.com>
2025-01-21 20:36:12 -0300

    wit: create dictionary's related entities

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -42,4 +43,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function wordHistory(): BelongsToMany
+    {
+        return $this->belongsToMany(Word::class, 'user_word_history')->withPivot(['created_at']);
+    }
+
+    public function favoriteWords(): BelongsToMany
+    {
+        return $this->belongsToMany(Word::class, 'user_word_favorite')->withPivot(['created_at']);
+    }
 }
